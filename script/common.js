2b7b37418a328c67fcb37362fa521332021c629b
Hiruge <okita1110@outlook.com>
2024-07-19 10:43:26 +0900

    [update] 最新の状況を反映

--- script/common.js
+++ script/common.js
@@ -1,5 +1,10 @@
 'use strict';
 $(document).ready(function() {
+    // ハンバーガーメニューの開閉
+    $('.hamburger').on('click', function() {
+        $('.hamburger, .slide-menu').toggleClass('active');
+    });
+
     // ページトップへ戻るボタン
     $('.pageTop-button').on('click', function() {
         event.preventDefault();
--- script/index.js
+++ script/index.js
@@ -1,12 +1,48 @@
 'use strict';
 $(document).ready(function() {
-    // 変換前レシピ入力欄の出力
-        // 変換前レシピの投数が変化したら入力内容を取得して、投数分だけレシピ入力<input>欄を生成する
-        // ただし、2投目まではデフォルトで表示しておき、足りない分を生成･増えすぎたら削除する
-    $('#pour-times-input').on('change', function(){
-        const InputPourTimes = $('#pour-times-input').val();
-        const CurrentPourTimes = $('.origin-process').children().length;
+    // アイスモードの切り替え(チェックボックスのON/OFFで表示を切り替える)
+    $('#ice-check').on('change', function(){
+        // 当初は.show()と.hide()で表示を切り替えていたが、フォームの自動フォーカスが効かなくなるため、html()で中身を書き換えることにした
+        let iceInputDivText= `
+            <label for="ice-input">変換前レシピの氷量(g): </label>
+            <input type="text" id="ice-input" maxlength="3" onkeyup="nextField(this)"> g
+        `;
+        if($(this).prop('checked')){
+            $('.ice-input-div').html(iceInputDivText);
+            $('.ice-mode-show').show();
+            $('html, header, footer').addClass('ice-mode'); // アイスモード時のスタイル変更用
+        }else{
+            $('.ice-input-div').html('');
+            $('.ice-mode-show').hide();
+            $('html, header, footer').removeClass('ice-mode');  // アイスモード時のスタイル変更用
+        }
+    });
 
+    // プリセットレシピ呼び出し
+        // プリセットレシピ用のデータをオブジェクトで持っておき、それを呼び出す
+    const PresetRecipes = {
+        "4-6method" : {
+            bean_g: '20',
+            water_ml: '300',
+            recipe: [['0:00', 60], ['0:45', 120], ['1:30', 180], ['2:10', 240], ['2:45', 300]]
+        },
+
+        "4-6method-ice" : {
+            bean_g: '20',
+            ice_g: '80',
+            water_ml: '150',
+            recipe: [['0:00', 30], ['0:40', 60], ['1:10', 90], ['1:40', 120], ['2:10', 150]]
+        },
+
+        "hatakeyamasan" : {
+            bean_g: '15',
+            water_ml: '230',
+            recipe: [['0:00',30], ['0:30', 120], ['1:00', 150], ['1:20', 190], ['1:40', 230]]
+        }        
+        
+    };
+
+    function originRecipeFormLengthAdjuster(InputPourTimes, CurrentPourTimes){
         if (InputPourTimes > CurrentPourTimes) {
             for (let i = CurrentPourTimes; i < InputPourTimes; i++) {                
                 let processInput = `
@@ -23,6 +59,63 @@ $(document).ready(function() {
                 $(`.pour-step${i}`).remove();
             }
         }
+    }
+
+    function selectedButtonDarkener(LightButtonsClass, DarkButtonId){
+        $(`.${LightButtonsClass}`).removeClass('selected-button');
+        $(`#${DarkButtonId}`).addClass('selected-button');
+    }
+
+    function presetActivate(PresetId){
+        // PresetIdがPresetRecipesに存在するか確認して、存在すればフォームに反映
+        if(PresetRecipes[PresetId]){
+            const SelectedRecipe = PresetRecipes[PresetId];
+            selectedButtonDarkener('preset-button', PresetId);
+
+            // 変換前レシピの入力欄を生成   
+            $('#pour-times-input').val(SelectedRecipe.recipe.length);
+            const InputPourTimes = $('#pour-times-input').val();
+            const CurrentPourTimes = $('.origin-process').children().length;
+            originRecipeFormLengthAdjuster(InputPourTimes, CurrentPourTimes);
+
+            // プリセットレシピの内容をフォームに反映
+            // アイス用のレシピの場合は、アイスモードをONにする
+            if(SelectedRecipe.ice_g){
+                $('#ice-check').prop('checked', true).change();
+                $('#ice-input').val(SelectedRecipe.ice_g);
+            }else{
+                $('#ice-check').prop('checked', false).change();
+            }
+            $('#bean-input').val(SelectedRecipe.bean_g);    // 豆量転記
+
+            for (let i = 1; i <= InputPourTimes; i++) {
+                let minutes = SelectedRecipe.recipe[i-1][0].split(':')[0];
+                let seconds = SelectedRecipe.recipe[i-1][0].split(':')[1];
+                let pour_ml = SelectedRecipe.recipe[i-1][1];
+                $(`.pour-step${i}`).children('.minutes').val(minutes);
+                $(`.pour-step${i}`).children('.seconds').val(seconds);
+                $(`.pour-step${i}`).children('.pour-ml').val(pour_ml);
+            }
+
+            // プリセットレシピの比率を計算して表示
+            const OriginRatio = brewParameterCompleter([SelectedRecipe.bean_g, SelectedRecipe.water_ml, '']);
+            $('#origin-ratio').html(OriginRatio);
+        }else{
+            console.log('Error[presetActivate]: プリセットIDが不正です');
+            return 'Error';
+        }
+    }
+
+    $('.preset-button').on('click', function(){
+        const PresetId = $(this).attr('id');
+        presetActivate(PresetId);
+    });
+
+    // 変換前レシピ入力欄の出力
+    $('#pour-times-input').on('change', function(){
+        const InputPourTimes = $('#pour-times-input').val();
+        const CurrentPourTimes = $('.origin-process').children().length;
+        originRecipeFormLengthAdjuster(InputPourTimes, CurrentPourTimes);
     });
 
     // 変換前レシピの入力補助
@@ -48,14 +141,14 @@ $(document).ready(function() {
         const PourTimes = $('#pour-times-input').val();
         const OriginSumWater = $(`.pour-step${PourTimes}`).children('.pour-ml').val();
         const OriginBean = $('#bean-input').val();
-        
-        const OriginRatio = brewParameterCompleter([/*豆量=*/OriginBean, /*総湯量=*/OriginSumWater, /*比率=*/'']);
-        $('#origin-ratio').html(OriginRatio);
+        if (OriginBean && OriginSumWater) {
+            const OriginRatio = brewParameterCompleter([/*豆量=*/OriginBean, /*総湯量=*/OriginSumWater, /*比率=*/'']);
+            $('#origin-ratio').html(OriginRatio);
+        }
     });
 
 
     // 変換目標入力欄の入力補助(豆量･総湯量･比率のどれか2つを入力すると、残り1つを計算して補完する)
-    // Todo: 比率入力時などに顕著だが、フォームに値が既に入っていると変換がうまくいかない(一旦手動で消さないといけない)ので新規入力の方を優先して上書きできるようにしたい
     $('.targetProcessComplete_argument').on('change', function(){
         let targetBean = $('#bean-target').val();
         let targetWater = $('#water-target').val();
@@ -79,10 +172,10 @@ $(document).ready(function() {
 
     // 変換目標入力欄のクリア
     $('.clear-button').on('click', function(){
+        event.preventDefault(); // ページ遷移を防ぐ
         $('#bean-target').val('');
         $('#water-target').val('');
         $('#ratio-target').val('');
-        event.preventDefault(); // ページ遷移を防ぐ
     });
 
 
@@ -104,43 +197,43 @@ $(document).ready(function() {
 
         if(errorMassage !== defaultMessage){
             window.alert(errorMassage);
+            return 'Error';
         }
     }
 
     function recipeConverter(pourTimes, convertRate) {
-        const DefaultProcessOutput = `
+        const DefaultOutput = `
             <tr>
                 <th>経過時間</th>
                 <th>注湯量</th>
                 <th>総注湯量</th>
             </tr>
         `;
-        let processOutput = DefaultProcessOutput;
-        let totalWater_ml = 0;
+        let Output = DefaultOutput;
+        let totalWater_mls=[0], minutes=["0"], seconds=["00"], input_pour_mls=[0],convertedPour_mls=[0];
+        // todo:アイスモード時、氷量も含めた合計量として変換を行えるようにする
         for (let i = 1; i <= pourTimes; i++) {
             // todo:算出とフォーマットが並行して行われてしまっているので、まず算出し、フォーマット用の関数に渡して整形するようにしたい
-            let minutes = String($(`.pour-step${i}`).children('.minutes').val()).padStart(2, '0');
-            let seconds = String($(`.pour-step${i}`).children('.seconds').val()).padStart(2, '0');
-            let input_pour_ml = $(`.pour-step${i}`).children('.pour-ml').val();
-            // ひとつ前の総湯量を記録しておくバッファ(各投での注湯量を計算するために必要)
-            let totalWater_ml_buf = Math.trunc(totalWater_ml); 
-            totalWater_ml = Math.trunc(input_pour_ml * convertRate);
+            minutes.push(String($(`.pour-step${i}`).children('.minutes').val()).padStart(2, '0'));
+            seconds.push(String($(`.pour-step${i}`).children('.seconds').val()).padStart(2, '0'));
+            input_pour_mls.push($(`.pour-step${i}`).children('.pour-ml').val());
+            totalWater_mls.push(Math.trunc(input_pour_mls[i] * convertRate));
             // 蒸らし固定ONの場合、1投目の総湯量は固定(元レシピの1投目の総湯量と同じ)
             if (i === 1 && $('#steep-check').prop('checked')) {
-                totalWater_ml = $(`.pour-step1`).children('.pour-ml').val();
+                totalWater_mls[1] = $(`.pour-step1`).children('.pour-ml').val();
             }
 
             // 各投での注湯量を計算(総湯量 - ひとつ前の総湯量)
-            let convertedPour_ml = Math.trunc(totalWater_ml - totalWater_ml_buf);
-            processOutput += `
-                <tr class="output-recipe-step${i}">
-                    <td>${minutes}:${seconds}</td>
-                    <td>${convertedPour_ml} ml</td>
-                    <td>${totalWater_ml} ml</td>
+            convertedPour_mls.push(Math.trunc(totalWater_mls[i] - totalWater_mls[i-1]));
+            Output += `
+                <tr>
+                    <td>${minutes[i]}:${seconds[i]}</td>
+                    <td>${convertedPour_mls[i]} ml</td>
+                    <td>${totalWater_mls[i]} ml</td>
                 </tr>
             `;
         }
-        return processOutput;
+        return Output;
     }
 
     // レシピの変換･変換後レシピの出力
@@ -152,7 +245,7 @@ $(document).ready(function() {
         let originWaterTotal_ml = $(`.pour-step${pourTimes}`).children('.pour-ml').val();
 
         let targetBean_g, targetWaterTotal_ml, convertRate;
-        if($('#magnification').val() !== ''){ // 変換率が手動入力されている場合は、それを採用して変換する
+        if($('#magnification').val()){ // 変換率が手動入力されている場合は、それを採用して変換する
             convertRate = $('#magnification').val();
             targetBean_g = $('#bean-input').val()*convertRate;
             targetWaterTotal_ml = originWaterTotal_ml*convertRate;
@@ -162,12 +255,20 @@ $(document).ready(function() {
             convertRate = targetWaterTotal_ml / originWaterTotal_ml;
         }
 
-        // 入力エラー検知関数に処理を投げて、エラーがあればアラートを出す
-        inputError_Detector([pourTimes, originWaterTotal_ml, targetBean_g, targetWaterTotal_ml])
+        // 入力エラー検知関数に処理を投げて、エラーがあればアラートを出して処理を中断
+        if(inputError_Detector([pourTimes, originWaterTotal_ml, targetBean_g, targetWaterTotal_ml])=='Error'){
+            return;
+        }
 
-        // 変換後の豆量と総湯量を転記
-        $('.bean-output').text(targetBean_g);
-        $('.water-output').text(targetWaterTotal_ml);
+        // 変換後の豆量と総湯量を転記(小数点第一位まで表示)
+        $('.bean-output').text(Math.trunc(targetBean_g*10)/10);
+        $('.water-output').text(Math.trunc(targetWaterTotal_ml*10)/10);
+
+        // 氷量が入力されている場合、変換後の氷量を算出・出力
+        let convertedIce_g = Math.trunc($('#ice-input').val()*convertRate);
+        if(convertedIce_g){
+            $('.ice-output').text(convertedIce_g);
+        }
 
         // 変換後のレシピを算出・出力
         const ConvertedRecipe = recipeConverter(pourTimes, convertRate);
@@ -193,49 +294,52 @@ $(document).ready(function() {
     
     // 時間を表示する関数
     function displayTime() {
-      const CurrentTime = new Date(Date.now() - startTime + stopTime);
-      const M = String(CurrentTime.getMinutes()).padStart(2, '0');
-      const S = String(CurrentTime.getSeconds()).padStart(2, '0');
-    
-      Time.textContent = `${M}:${S}`;
-      timeoutID = setTimeout(displayTime, 10);
+        const CurrentTime = new Date(Date.now() - startTime + stopTime);
+        const M = String(CurrentTime.getMinutes()).padStart(2, '0');
+        const S = String(CurrentTime.getSeconds()).padStart(2, '0');
+        
+        Time.textContent = `${M}:${S}`;
+        timeoutID = setTimeout(displayTime, 10);
     }
     
     // スタートボタンがクリックされたら時間を進める
-    StartButton.addEventListener('click', () => {
-      StartButton.disabled = true;
-      StopButton.disabled = false;
-      ResetButton.disabled = true;
-      startTime = Date.now();
-      displayTime();
+    $(`#${StartButton.id}`).on('click', function() {
+        StartButton.disabled = true;
+        StopButton.disabled = false;
+        ResetButton.disabled = true;
+        // 選択中のボタンは暗くする
+        selectedButtonDarkener('timer-button', StartButton.id);
+        
+        startTime = Date.now();
+        displayTime();
     });
     
     // ストップボタンがクリックされたら時間を止める
-    StopButton.addEventListener('click', function() {
-      StartButton.disabled = false;
-      StopButton.disabled = true;
-      ResetButton.disabled = false;
-      clearTimeout(timeoutID);
-      stopTime += (Date.now() - startTime);
+    $(`#${StopButton.id}`).on('click', function() {
+        StartButton.disabled = false;
+        StopButton.disabled = true;
+        ResetButton.disabled = false;
+        selectedButtonDarkener('timer-button', StopButton.id);
+
+        clearTimeout(timeoutID);
+        stopTime += (Date.now() - startTime);
     });
     
     // リセットボタンがクリックされたら時間を0に戻す
-    ResetButton.addEventListener('click', function() {
-      StartButton.disabled = false;
-      StopButton.disabled = true;
-      ResetButton.disabled = true;
-      Time.textContent = '00:00';
-      stopTime = 0;
+    $(`#${ResetButton.id}`).on('click', function() {
+        StartButton.disabled = false;
+        StopButton.disabled = true;
+        ResetButton.disabled = true;
+        selectedButtonDarkener('timer-button', ResetButton.id);
+        Time.textContent = '00:00';
+        stopTime = 0;
     });
 
-
-    
-    
-    // ストップウォッチ部分のトグル機能
+    // トグル機能
     // Tips:JavaScriptで動かすと負荷がかかるので、必要に迫られればCSSでの実装も検討(滑らかに閉じるのが難しかったので現在はJSで実装)
-    $('.timer-item').hide();
+    $('.accordion-item').hide();
     $('.accordion-head').on('click', function() {
-        $('.accordion-toggle').toggleClass('rotate-90');
-        $('.timer-item').slideToggle(300);
+        $(this).children('.accordion-toggle').toggleClass('rotate-90');
+        $(this).next().children('.accordion-item').slideToggle(300);
     });
 });
